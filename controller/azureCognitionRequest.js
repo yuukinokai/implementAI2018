0ec586685067b7b16318d9919c7b80b3a3c62897
Sophie Deng <sophie_deng97@hotmail.com>
2018-09-29 15:34:57 -0400

    added html

---
+++ controller/azureCognitionRequest.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const request = require('request');
+
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = 'fb2a1d4c0ddc4948b6c15bbf8c774423';
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase =
+    'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze';
+
+const imageUrl =
+    'http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+// Request parameters.
+const params = {
+    'visualFeatures': 'Categories,Description,Color',
+    'details': '',
+    'language': 'en'
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
